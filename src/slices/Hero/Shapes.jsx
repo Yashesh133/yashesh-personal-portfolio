b5e47ffbcd1e9f457de50c9759a1b73dffbd02b4
Yashesh133 <yashcreative133@gmail.com>
2024-01-05 17:40:49 +0530

    sound added new

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -62,9 +62,14 @@ function Geometries() {
   ];
 
   const soundEffects = [
+    new Audio("./sounds/hit1.ogg"),
     new Audio("./sounds/hit2.ogg"),
     new Audio("./sounds/hit3.ogg"),
     new Audio("./sounds/hit4.ogg"),
+    new Audio("./sounds/hit5.ogg"),
+    new Audio("./sounds/hit6.ogg"),
+    new Audio("./sounds/hit7.ogg"),
+    new Audio("./sounds/hit8.ogg"),
   ];
 
   const materials = [
